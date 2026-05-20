592f48b272424be1e782db4a18f337fc476f9fb2
wes <wes@wesk.tech>
2026-05-20 14:52:37 -0400

    fix file reader

--- src/root.zig
+++ src/root.zig
@@ -8,8 +8,10 @@ pub fn runFile(_: std.mem.Allocator, filename: []const u8) !void {
     const cwd = std.Io.Dir.cwd();
     const file = try std.Io.Dir.openFile(cwd, io, filename, .{});
     defer file.close(io);
+    //var buffer: std.ArrayList(u8) = .empty;
+    //defer buffer.deinit(allocator);
     var buffer: [10]u8 = undefined;
-    var file_reader = file.bufferedReader(io, &buffer);
+    var file_reader = std.Io.File.reader(file, io, &buffer);
     var reader = &file_reader.interface;
     try reader.fillMore();
     std.debug.print("{s}\n", .{reader.buffered()});
